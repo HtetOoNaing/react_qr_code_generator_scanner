bf846f9da76f3fb18ba65da3ff674deb1c0e4ddd
HtetOoNaing <htetoonaing.ycc@gmail.com>
2020-09-29 12:31:28 +0630

    update sharphway link to readme

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import GenerateQr from "./GenerateQr";
+
+function App() {
+  return (
+    <div className="App">
+      <GenerateQr />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ScanQr.js
@@ -0,0 +1,7 @@
+import React from "react";
+import QRCode from "qrcode.react";
+const GenerateQr = () => {
+  return <QRCode value="http://facebook.github.io/react/" />;
+};
+
+export default GenerateQr;
