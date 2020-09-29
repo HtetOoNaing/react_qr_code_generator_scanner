e15424cc9a562aefeba3a94392bae1eaf70d92fb
HtetOoNaing <htetoonaing.ycc@gmail.com>
2020-09-29 16:07:55 +0630

    Updates

---
+++ precache-manifest.57c265ed31624e4573c98d359ff8815d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbca1066bd4c85e398eb782e51e0426",
+    "url": "/react_qr_code_generator_scanner/index.html"
+  },
+  {
+    "revision": "f34169b8db8c2cc75cfe",
+    "url": "/react_qr_code_generator_scanner/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6dd0120fac4b959e3698",
+    "url": "/react_qr_code_generator_scanner/static/js/2.fb3e56e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_qr_code_generator_scanner/static/js/2.fb3e56e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f34169b8db8c2cc75cfe",
+    "url": "/react_qr_code_generator_scanner/static/js/main.d02467d1.chunk.js"
+  },
+  {
+    "revision": "eb4858ff008966af08fd",
+    "url": "/react_qr_code_generator_scanner/static/js/runtime-main.af80d25b.js"
+  }
+]);
